e224edbaf6756dedf47f30a0dd80f1da74184d22
MichaelPile <mjpile@gmail.com>
2024-02-14 16:32:51 -0800

    classifications are now dictionaries

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from config import db
+import json
 #from rich import print
 
 class Cape(db.Model):
@@ -6,14 +7,23 @@ class Cape(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     cape_name = db.Column(db.String, unique = True) #other constraints too, unique- no two rows can have same name
-    classification = db.Column(db.String)
+    classification_json = db.Column(db.Text)
     powers = db.Column(db.String)
     allignment = db.Column(db.String)
 
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id')) #foreign key links each Cape (row) to a team in a one to many relationship
 
-    def __repr__(self): #changes how a Cape is printed
-        return f'{self.id}: Name: {self.cape_name} Classification: {self.classification} Powers: {self.powers}'
+    def __init__(self, cape_name, classification, powers, allignment, team_id):
+        self.cape_name = cape_name
+        self.classification_json = json.dumps(classification)
+        self.powers = powers
+        self.allignment = allignment
+        self.team_id = team_id
+
+
+    @property
+    def classification(self):
+        return json.loads(self.classification_json)
 
 
 class Team(db.Model):
@@ -25,3 +35,15 @@ class Team(db.Model):
 
     def __repr__(self):
         return f'{self.id}: Name: {self.team_name} allignment: {self.allignment}'
+
+class ActionEffect(db.Model):
+    __tablename__ = 'action_effects'
+
+    id = db.Column(db.Integer, primary_key = True)
+    quality_name = db.Column(db.String, unique = True)
+    ambush = db.Column(db.Integer) 
+    outwit = db.Column(db.Integer)
+    overpower = db.Column(db.Integer)
+    convince = db.Column(db.Integer)
+    extort = db.Column(db.Integer)
+    bribe = db.Column(db.Integer)
